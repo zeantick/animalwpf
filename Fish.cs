e563c9eba32f4dfc299023a6b5d1e937254796b9
Zoldnik <aivanechek2112@gmail.com>
2025-10-19 22:05:16 +0300

    Add files via upload

---
+++ Fish.cs
@@ -0,0 +1,10 @@
+﻿using animalwpf.Models;
+using animalwpf.Interfaces;
+
+public class Fish : Animal, IFlyable
+{
+    public override string MakeSound() => "Blub blub!";
+    public void Fly()
+    {
+    }
+}
---
+++ Racoons.cs
@@ -0,0 +1,8 @@
+﻿using animalwpf.Models;
+using animalwpf.Interfaces;
+
+public class Raccoon : Animal, ICrazyAction
+{
+    public override string MakeSound() => "Squeak!";
+    public string ActCrazy() => $"{Name} found a shiny object!";
+}
